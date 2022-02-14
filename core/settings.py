0ddb5f65e4278b7f5d6c5239e97ece54b8dd78dd
Anthoni <softwaretoni21@gmail.com>
2022-02-13 20:19:31 -0500

    update2

--- core/settings.py
+++ core/settings.py
@@ -109,7 +109,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
 
-    'https://frontend-paideia.herokuapp.com/',
+    'https://frontend-paideia.herokuapp.com',
 ]
 
 
